b333d0e65a6bfe885283811f596d8c95229d5105
tutrungtranvn <tutrung.leo@gmail.com>
2023-09-20 16:33:09 +0700

    new file:   datadog_values.yaml 	modified:   setup.sh
    Changes not staged for commit:
    	modified:   kind-cluster.yaml
    	modified:   pvc.yaml
    
    Untracked files:
    	.DS_Store
    	.setup.sh.swp
    	data/
    	logs_volume.yaml

--- setup.sh
+++ setup.sh
@@ -32,3 +32,5 @@ helm repo add apache-airflow https://airflow.apache.org
 helm repo update
 helm search repo airflow
 helm install airflow apache-airflow/airflow --namespace airflow --debug -f values.yaml
+
+## (Optional) - Install DataDog Agent  
